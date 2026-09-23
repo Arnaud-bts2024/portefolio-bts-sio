7e80896f7e0454bba825aee53f587bb6c91b68b3
Arnaud-bts2024 <hollebecque.arnaud@gmail.com>
2026-09-24 00:36:08 +0200

    Update CV link to use new file name

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ function initNavbar() {
                         <li class="dropdown">
                             <a href="parcours.html">Mon parcours</a>
                             <ul class="submenu">
-                                <li><a href="documents/CV Arnaud HOLLEBECQUE.pdf" target="_blank" rel="noopener noreferrer">Mon CV</a></li>
+                                <li><a href="documents/cv.pdf" target="_blank" rel="noopener noreferrer">Mon CV</a></li>
                                 <li><a href="projets.html">Mes TP</a></li>
                                 <li><a href="https://online.fliphtml5.com/fvfkdx/uksp/" target="_blank" rel="noopener noreferrer">Mes Certifications</a></li>
                                 <li><a href="stages.html">Mes Stages</a></li>
